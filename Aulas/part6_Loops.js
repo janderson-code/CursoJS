4c03389d812e1381acd5fd5be6de359afa145df9
janderson-code <jbgfilebarbosa@gmail.com>
2021-08-23 07:02:21 -0300

    Create part6_Loops.js

---
+++ Aulas/part6_Loops.js
@@ -0,0 +1,11 @@
+//Loops
+//Quick and easy way to do something repeatedly
+
+//while loop
+
+let numberOfLoops = 0;
+
+while(numberOfLoops < 10){
+	console.log("I am Looping");
+	numberOfLoops+=1;
+}
