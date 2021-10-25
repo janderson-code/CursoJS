16fe1654924375089a7073d1c300c1cdcd689333
janderson-code <jbgfilebarbosa@gmail.com>
2021-10-25 04:42:39 -0300

    Update part8_Events.js

--- Aulas/part8_Events.js
+++ Aulas/part8_Events.js
@@ -19,6 +19,8 @@
 //---------------------------------------
 
 //Evento que gerar um pop up ao clicar numa pagina
+//no caso o addEvent precisa de dois param um com evento e o outro com a function
+
 
 document.addEventListener("click", function(){
 	console.log("Clicado");
@@ -47,3 +49,82 @@ clickDiv.addEventListener("click",function(event){
 
 // 9:00
 
+//Evento que gerar um alert dialog ao clicar numa pagina usando ID
+const clickableButton = clickDiv.querySelector("button");
+
+// gerando alerta com info do typo do evento ao clicar no button 
+clickableButton.addEventListener("click", function(){
+	alert(event.type);
+});
+
+//Chamando esta function pelo onclick de um button no html
+function handleClick(){
+	alert("clicked");
+}
+// Gera o alerta quando o button esta com cursos em focus 
+clickableButton.addEventListener("focus",function(event){
+	console.log(event);
+});
+// Gera o alerta no console quando o button esta com cursos perde o focus
+clickableButton.addEventListener("focusout",function(event){
+	console.log(event.type);
+
+});
+
+// criando função
+function logEvent(event){
+	console.log(event.type);
+}
+
+function logEvent2(e){
+	if (e.type ==="click"){
+		console.log("click");
+	}
+	if (e.type ==="focus"){
+		console.log("Focus");
+	}
+	else{
+		return false;
+	}
+}
+
+// Também funciona como variavel e arrow antes do addEvent
+
+const logEvent3 = (e) =>{
+	if (e.type ==="click"){
+		console.log("click");
+	}
+	if (e.type ==="focus"){
+		console.log("Focus");
+	}
+	else{
+		return false;
+	}
+
+}
+
+
+
+//Colocando função como parametro e chamando não precisa()
+clickableButton.addEventListener("click",logEvent4);
+// Gera o alerta quando o button esta com cursos em focus 
+clickableButton.addEventListener("focus",logEvent2);
+// Gera o alerta no console quando o button esta com cursos perde o focus
+clickableButton.addEventListener("focusout",logEvent2);
+
+///////////////////////////////////////////////////////
+
+/*Chama o logEvent4 no primeiro click*/
+clickableButton.addEventListener("click",logEvent4);
+
+
+function logEvent4(e){
+	//altera o nome do button plz no click
+	clickableButton.innerText = "Plz no click";	
+    //mostra no console devtools o typo click
+	console.log(e.type);
+	// remove o evento do tipo 
+	clickableButton.removeEventListener("click",logEvent4);
+	//Não mostra no console o event click
+	console.log(e.type);
+}
