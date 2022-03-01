f5a368ce0181cc5e9bbe4d7e4335bb1617bfa702
janderson-code <jbgfilebarbosa@gmail.com>
2022-03-01 14:01:40 -0300

    update part_18_MapsAndSets.js

---
+++ Aulas/part_18_MapsAndSets.js
@@ -0,0 +1,13 @@
+//Map Object
+
+/*
+ - Used for simple key/value pairs
+ - Any value can be used for either a key or value
+   (i.e object , string, function)
+ - Reasons for using Map over Object:
+   -- Object prototyṕe has default keys that might conflict with
+   own keys
+   -- Keys of map can be anything, where as with objects,have to be
+   string of symbol
+   -- Can  easily get the size of Map 
+*/
