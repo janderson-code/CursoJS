95fdc46a57ac94488a74e8f535a1b3814a035488
janderson-code <jbgfilebarbosa@gmail.com>
2022-03-06 15:35:51 -0300

    Update part_18_MapsAndSet.js

--- Aulas/part_18_MapsAndSets.js
+++ Aulas/part_18_MapsAndSets.js
@@ -84,9 +84,84 @@ console.log(anotherMap2);
 /*
 	- Lets you store unique values of any type
 	- Each Element is unique
+	 Não printa nem seta valor duplicado
 */
 
 const mySet = new Set();
-mySet.add("Bob")
-console.log(mySet);
+const obj123 = {a:1,b:2}
+const Obj456 = {x:1,y:2,z:3};
+const myString = "Hello there :D";
+const aBool = true;
+mySet.add("Bob");
+mySet.add(true);
+mySet.add("Bob"); // Não printa nem seta valor duplicado
 
+
+mySet.add(obj123);
+mySet.add(Obj456);
+mySet.add(myString);
+mySet.add(aBool);
+console.log(mySet);
+console.log(mySet.size); //5
+
+mySet.delete(obj123);
+
+console.log("--forEach--");
+
+mySet.forEach((s)=>{
+	console.log(s);
+});
+
+mySet.forEach((s)=>{
+	console.log(`Set item ${s}`);
+});
+
+mySet.forEach((s)=>{
+	if (typeof s === "object") {
+		console.log("Got object");
+	}else{
+		console.log(`Set item ${s}`);
+	}
+});
+
+
+for(const val of mySet.values()){
+	console.log(val);
+}
+
+for(const keys of mySet.keys()){
+	console.log(keys);
+}
+
+
+for(const [keys,value] of mySet.entries()){
+	console.log(`Key => ${keys} and values => ${value}`);
+}
+
+
+
+//Convert set to Array and vice versa
+
+const arrFromSet = Array.from(mySet);
+arrFromSet.push(myString);
+console.log(arrFromSet);
+
+const arrFromMap = Array.from(anotherMap2);
+console.log(arrFromMap); // Line 78
+
+const arrFromString = Array.from("Hello there");
+console.log(arrFromString);
+
+
+//Array to set
+
+const myArr123 = [1,2,3,4,5];
+const myArr456 = ["janderson",false,{a:3},4,5];
+const myArr111 = [1,1,1,1,2];
+const anotherSet = new Set(myArr123);
+const anotherSet2 = new Set(myArr456);
+const anotherSet3 = new Set(myArr111);
+console.log(anotherSet);
+console.log(anotherSet2);
+console.log(anotherSet3);// Não printa nem seta valor duplicado
+console.log(anotherSet3.has(41));
